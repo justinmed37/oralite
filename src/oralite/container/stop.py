dc03c99f3be216ebd5129bcf3e4abe6a9e0f38fe
jmad <justinmed37@gmail.com>
2024-04-11 15:47:42 -0400

    MORE_TESTS

--- src/oralite/container/stop.py
+++ src/oralite/container/stop.py
@@ -5,9 +5,9 @@ from .client import *
 client_function = client.stop_container_instance
 
 # Pass the client function and container_id to the function wrapper
-def main(func):
-    response = wrapper(func, container_id)
+def main(func, id):
+    response = wrapper(func, id)
     print(response.data) # print output when executed from cli
 
 if __name__ == "__main__":
-    main(client_function)
+    main(client_function, container_id)
---
+++ tests/container/test_stop.py
@@ -0,0 +1,21 @@
+# Test the container client using a container instance from the model
+import pytest
+
+from src.oralite.core import wrapper
+from src.oralite.container.stop import client_function, main
+from src.oralite.container.client import model_containers
+from src.oralite.core import model
+
+
+@pytest.fixture
+def model_container_ocid():
+    id = model_containers[0]["id"]
+    return id
+
+def test_container_ocid_fixture(model_container_ocid):
+    assert model_containers
+    assert model_container_ocid
+
+def test_client_function():
+    assert client_function
+    assert main
